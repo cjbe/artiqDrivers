ccc168601459d95cd33fec4d99f99def5f381ed6
oldlabMaster <none>
2017-03-20 12:05:15 +0000

    add warning comment about phase accumulation during profile switching

--- artiqDrivers/devices/arduinoDds/uwaveDdsWrapper.py
+++ artiqDrivers/devices/arduinoDds/uwaveDdsWrapper.py
@@ -2,7 +2,8 @@ from artiq.language.core import *
 
 
 class UwaveDdsWrapper:
-    """Wraps an Arduino DDS class to allow profiles to be set in logical frequencies (detunings from zero field) rather than the physical frequencies that are the input to the mixup chain"""
+    """Wraps an Arduino DDS class to allow profiles to be set in logical frequencies (detunings from zero field) rather than the physical frequencies that are the input to the mixup chain
+    !!! The profiles are switched sequentially by profile select line with 200ns delay. When switching profiles care has to be taken that the intermediate profiles that are enabled for a short time during the switching process have the same frequency as the previously selected one, otherwise the phase will become scrambled!!! """
     def __init__(self, dmgr, device, LOfrequency):
         self.core = dmgr.get("core")
 
