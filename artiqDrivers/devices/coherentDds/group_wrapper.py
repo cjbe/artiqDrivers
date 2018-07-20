53db52d3ba10e49e72f6b1b4ea831cd5d3ef7043
oldlabMaster <none>
2018-07-20 18:46:11 +0100

    coherentDds: added method to reset phase via spi, removed extraneous spi.write line, and refactored

--- artiqDrivers/devices/coherentDds/group_wrapper.py
+++ artiqDrivers/devices/coherentDds/group_wrapper.py
@@ -47,46 +47,9 @@ class DdsGroup:
 
             spi_dev = spi_devices[dev_name]
 
-            channel_cls = DdsChannel(self.core, dds_dev, spi_dev, ch, \
-                                    self._write_profile_select,\
-                                    self._write_pulse_enable)
+            channel_cls = DdsChannel(self.core, dds_dev, spi_dev, ch,\
+                                    self._spi_write)
             setattr(self, channel, channel_cls)
-
-
-    @kernel
-    def _write_profile_select(self, spi, ch, profile,delay = True):
-        """Set the profile select for a given spi device and channel number."""
-        # Wire format:
-        # cs low, clock in 8 bit word, cs high
-        # bits 7-6 : mode, 0=set profile select, 1=set pulse enable
-        # bits 5-4 : channel, 0-3 for DDS channel 0-3
-        # bits 3-0 : mode dependant data
-        # if mode=0 : data[2:0] is the profile select vector
-        # if mode=1 : data[0] is the pulse enable line
-        # The DDS control signals take effect on the rising edge of cs.
-
-        data = 0 << 6
-        data += (ch & 3) << 4
-        data += profile & 7
-        spi.write(data<<24)
-        self._spi_write(spi,data,delay = delay)
-
-    @kernel
-    def _write_pulse_enable(self, spi, ch, enable):
-        """Set the profile select for a given spi device and channel number."""
-        # Wire format:
-        # cs low, clock in 8 bit word, cs high
-        # bits 7-6 : mode, 0=set profile select, 1=set pulse enable
-        # bits 5-4 : channel, 0-3 for DDS channel 0-3
-        # bits 3-0 : mode dependant data
-        # if mode=0 : data[2:0] is the profile select vector
-        # if mode=1 : data[0] is the pulse enable line
-        # The DDS control signals take effect on the rising edge of cs.
-
-        data = 1 << 6
-        data += (ch & 3) << 4
-        data += enable & 1
-        self._spi_write(spi,data,delay = False)
 
     @kernel
     def _spi_write(self, spi, data, delay):
@@ -97,13 +60,12 @@ class DdsGroup:
 
 class DdsChannel:
     kernel_invariants = {"spi", "ch"}
-    def __init__(self, core, device, spi, channel, _write_profile_select, _write_pulse_enable):
+    def __init__(self, core, device, spi, channel, _spi_write):
         self.core = core
         self.dev = device
         self.spi = spi
         self.ch = channel
-        self._write_profile_select = _write_profile_select
-        self._write_pulse_enable =_write_pulse_enable
+        self._spi_write = _spi_write
 
     def set(self, frequency, profile=0, amplitude=1, phase=0):
         self.dev.setProfile(self.ch, profile, \
@@ -125,4 +87,55 @@ class DdsChannel:
     def pulse_enable(self,enable):
         self._write_pulse_enable(self.spi,self.ch,enable)
 
+    @kernel
+    def reset_phase(self):
+        self._write_reset_phase(self.spi)
 
+    @kernel
+    def _write_profile_select(self, spi, ch, profile,delay = True):
+        """Set the profile select for a given spi device and channel number."""
+        # Wire format:
+        # cs low, clock in 8 bit word, cs high
+        # bits 7-6 : mode, 0=set profile select, 1=set pulse enable
+        # bits 5-4 : channel, 0-3 for DDS channel 0-3
+        # bits 3-0 : mode dependant data
+        # if mode=0 : data[2:0] is the profile select vector
+        # if mode=1 : data[0] is the pulse enable line
+        # The DDS control signals take effect on the rising edge of cs.
+
+        data = 0 << 6
+        data += (ch & 3) << 4
+        data += profile & 7
+        self._spi_write(spi,data,delay = delay)
+
+    @kernel
+    def _write_pulse_enable(self, spi, ch, enable):
+        """Set the profile select for a given spi device and channel number."""
+        # Wire format:
+        # cs low, clock in 8 bit word, cs high
+        # bits 7-6 : mode, 0=set profile select, 1=set pulse enable
+        # bits 5-4 : channel, 0-3 for DDS channel 0-3
+        # bits 3-0 : mode dependant data
+        # if mode=0 : data[2:0] is the profile select vector
+        # if mode=1 : data[0] is the pulse enable line
+        # The DDS control signals take effect on the rising edge of cs.
+
+        data = 1 << 6
+        data += (ch & 3) << 4
+        data += enable & 1
+        self._spi_write(spi,data,delay = False)
+
+    @kernel
+    def _write_reset_phase(self, spi):
+        """Set the profile select for a given spi device and channel number."""
+        # Wire format:
+        # cs low, clock in 8 bit word, cs high
+        # bits 7-6 : mode, 0=set profile select, 1=set pulse enable
+        # bits 5-4 : channel, 0-3 for DDS channel 0-3
+        # bits 3-0 : mode dependant data
+        # if mode=0 : data[2:0] is the profile select vector
+        # if mode=1 : data[0] is the pulse enable line
+        # The DDS control signals take effect on the rising edge of cs.
+
+        data = 2 << 6
+        self._spi_write(spi,data,delay = True)
