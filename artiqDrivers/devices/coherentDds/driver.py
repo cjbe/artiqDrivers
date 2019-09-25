0b23e97e57dc39a4c1115c3c0c813bd3ed2856e7
vmsch <vm.schaefer@gmail.com>
2019-09-25 15:42:25 +0100

    coherent dds driver: fix clk frequency for pulse shaping

--- artiqDrivers/devices/coherentDds/driver.py
+++ artiqDrivers/devices/coherentDds/driver.py
@@ -11,14 +11,14 @@ class CoherentDds:
     ser = None;
     lsbAmp = 1.0 / 16383 # 0x3fff is maximum amplitude
     lsbPhase = 360.0 / 65536 # Degrees per LSB.
-  
-    def __init__(self, addr, clockFreq, internal_clock=False,
+
+    def __init__(self, addr, clockFreq, baudrate=115200, internal_clock=False,
                  incoherent_channels=[False, False, False, False]):
         # addr : serial port name
         # clockFreq : clock frequency in Hz
         # internal_clock: if true, use internal 1 GHz clock
         # incoherent_channels: array listing which channels coherence is disabled
-        self.ser = serial.Serial(addr, baudrate=115200)
+        self.ser = serial.Serial(addr, baudrate=baudrate)
         self.lsbFreq = clockFreq / (2**32);
         self.clockFreq = clockFreq
 
@@ -43,7 +43,7 @@ class CoherentDds:
 
     def send(self, data):
         self.ser.write(data.encode())
-    
+
     def identity(self):
         """Returns a string representing the firmware name and version"""
         self.send('idn?\n')
@@ -59,12 +59,12 @@ class CoherentDds:
             raise ValueError("DDS amplitude must be between 0 and 1")
         if freq < 0 or freq > 450e6: # This should be dependant on the clock frequency
             raise ValueError("DDS frequency must be between 0 and 450 MHz")
-        
+
         ampWord = int(round( amp * 0x3fff ))
         phaseWord = int(round( (phase % 360) / 360.0 * 0xffff ))
         freqWord = int(round( freq / self.lsbFreq ))
         self.setProfileWords(channel, profile, freqWord, phaseWord, ampWord)
-    
+
     def setProfileWords(self, channel, profile, freq, phase, amp): # Freq, phase, amp are all in units of lsb
         profile = int(profile) # have to do this, because artiq uses a special artiq.integer
         if channel < 0 or channel > 3 or not isinstance(channel, int):
@@ -77,7 +77,7 @@ class CoherentDds:
             raise ValueError("DDS phase word should be an integer between 0 and 0xffff")
         if freq < 0 or freq > 0xffffffff or not isinstance(freq, int):
             raise ValueError("DDS frequency word should be an integer between 0 and 0xffffffff")
-        
+
         self.send('setProfile {} {} {} {} {}\n'.format( channel, profile, freq, phase, amp) );
 
     def reset(self):
@@ -94,14 +94,14 @@ class CoherentDds:
             raise ValueError("DDS pulse shape channel should be an integer between 0 and 3")
         if len(shapeVec) < 1 or len(shapeVec) > 2048:
             raise ValueError("DDS pulse shape array length should be between 1 and 2048")
-        
+
         quantisedShapeVec = []
         for el in shapeVec:
             quantisedEl = round(el*0x3fff)
             if quantisedEl < 0 or quantisedEl > 0x3fff:
                 raise ValueError("DDS pulse shape points should all be between 0.0 and 1.0")
             quantisedShapeVec.append(quantisedEl)
-        
+
         self.send('setPulseShape {}\n'.format(shapeChannel))
         for i in range(len(quantisedShapeVec)):
             self.send('%d' % quantisedShapeVec[i]);
@@ -113,9 +113,9 @@ class CoherentDds:
         """Sets a sensible looking pulse shape with total duration 'duration' seconds. The duration must be between 0 and 10us"""
         if duration > 10e-6 or duration < 0.2e-6:
             raise ValueError("DDS pulse shape duration must be between 0.2us and 10us")
-                
+
         shapeVec = []
-        i_max = round(duration*200e6)
+        i_max = round(duration*250e6/2) # /2 because clock used is divided by 2, 250MHz is DDS sync clk
         for i in range(i_max):
             y = 0.209*math.log10( (math.sin((1+i)/float(i_max+1)*math.pi/2))**4 ) + 1
             if y < 0:
@@ -139,15 +139,15 @@ class CoherentDdsSim:
 
     def identity(self):
         return "coherentdds simulation"
-            
-    def resetPhase(self):  
+
+    def resetPhase(self):
         logger.warning("Resetting phase")
         pass
-    
+
     def setProfile(self, channel, profile, freq, phase=0.0, amp=1.0):
         logger.warning("Setting ch:p {}:{} to freq={}, phase={}, amp={}".format(channel,profile,freq,phase,amp))
         pass
-    
+
     def setProfileWords(self, channel, profile, freq, phase, amp): # Freq, phase, amp are all in units of lsb
         pass
 
@@ -156,6 +156,6 @@ class CoherentDdsSim:
 
     def setPulseShape(self, shapeChannel, shapeVec):
         pass
-    
+
     def ping(self):
         return True
