878ae184d45768e470956dc2b22342ca50c2f0c2
ion <thomas.harty@physics.ox.ac.uk>
2019-09-26 11:10:38 +0100

    raman interface: add correct qubit freq to aom freq conversion, add bichromatic tone

--- artiqDrivers/devices/coherentDds/raman_interface.py
+++ artiqDrivers/devices/coherentDds/raman_interface.py
@@ -49,21 +49,26 @@ class AOM:
 
 
 class RamanAOM(AOM):
-    def __init__(self,**kwargs):
-        self.rV_freq = 217.368568e6
+    def __init__(self,name,freq_range,order,dds,dds2=None):
+        self.rV_freq = 217.368555e6 # self.ms_diff+self.rH_freq-hfq.df_trans(mF4=4,mF3=3)
         self.ms_diff = 3.2e9 # master-slave Raman laser frequency difference
-        self.rH_freq = -109e6 # frequency of Rh, is -1st order
+        self.rH_freq = -109e6 # frequency of Rh, is -1st order, =self.rV_freq+hfq.df_trans(mF4=4,mF3=3)-self.ms_diff
+        self.rH2_freq = 217.309632e6 # frequency of Rh2, is +1st order, =self.rV_freq+hfq.df_trans(mF4=0,mF3=1)-self.ms_diff
 
-        super().__init__()
+        super().__init__(name,freq_range,order,dds,dds2=dds2)
 
-    def calculate_dds_frequency(self,frequency):
+    def calculate_dds_frequency(self,frequency,add_qubit_freq=True,on_clock=False):
         if self.name == 'rPara':
-            if addQubitFreq:
-                freqDDS = self.ms_diff+self.rH_freq-frequency
-            else:
+            if add_qubit_freq:
+                if on_clock:
+                    freqDDS = self.ms_diff+self.rH2_freq-frequency
+                else:
+                    freqDDS = self.ms_diff+self.rH_freq-frequency
+            else: # this is used for gates, where the laser detuning is only the motional mode frequency + delta_g
+            # works for both wobble and ms gate
                 freqDDS = self.rV_freq + frequency
         elif self.name == 'rH2':
-            freqDDS = self.ms_diff+self.rH_freq-frequency
+            freqDDS = self.rH2_freq
         elif self.name == 'rV':
             freqDDS = self.rV_freq
         else:
@@ -71,12 +76,12 @@ class RamanAOM(AOM):
         return freqDDS
 
 
-    def set(self, frequency, profile=0, amplitude=1, phase=0, addQubitFreq=True):
-        freqDDS = calculate_dds_frequency(frequency)
+    def set(self, frequency, profile=0, amplitude=1, phase=0, add_qubit_freq=True, on_clock=False):
+        freqDDS = self.calculate_dds_frequency(frequency,add_qubit_freq=add_qubit_freq,on_clock=on_clock)
         super().set(frequency=freqDDS, profile=profile, amplitude=amplitude, phase=phase)
 
-    def set2(self, frequency, profile=0, amplitude=1, phase=0, addQubitFreq=True):
-        freqDDS = calculate_dds_frequency(frequency)
+    def set2(self, frequency, profile=0, amplitude=1, phase=0, add_qubit_freq=True, on_clock=False):
+        freqDDS = self.calculate_dds_frequency(frequency,add_qubit_freq=add_qubit_freq,on_clock=on_clock)
         super().set2(frequency=freqDDS, profile=profile, amplitude=amplitude, phase=phase)
 
 
@@ -96,20 +101,17 @@ class RamanInterface:
         dds = dmgr.get(dds_device)
         self.core = dmgr.get("core")
 
-        assert(dds._rPara.get_lsb_freq() == dds._rV.get_lsb_freq() == dds._rH2.get_lsb_freq() == dds._rH2b.get_lsb_freq()) #4 channels of the same DDS. Sanity check, set up in device db
+        assert(dds._rPara.get_lsb_freq() == dds._rV.get_lsb_freq() == dds._rH2.get_lsb_freq() == dds._rParaB.get_lsb_freq()) #4 channels of the same DDS. Sanity check, set up in device db
         self.lsb = dds._rPara.get_lsb_freq()
 
-        #self.rPara = AOM("rPara",[140e6, 250e6],+1,dds._rPara) # BW 40 MHz, double pass -1 order
-        self.rPara = AOM("rPara",[0e6, 401e6],+1,dds._rPara)
-        self.rH2 = AOM("rH2",[175e6, 225e6],+1,dds._rH2,dds._rH2b) # BW 50 MHz, single pass -1 order . Here TTL switches second channel going to AOM, first channel is always on.
-        self.rV = AOM("rV",[175e6, 225e6],+1,dds._rV)
+        #self.rPara = AOM("rPara",[140e6, 250e6],+1,dds._rPara) # BW 40 MHz, single pass +1 order
+        self.rPara = RamanAOM("rPara",[200e6, 250e6],+1,dds._rPara,dds2=dds._rParaB)
+        self.rH2 = RamanAOM("rH2",[175e6, 225e6],+1,dds._rH2) # single pass +1 order
+        self.rV = RamanAOM("rV",[175e6, 225e6],+1,dds._rV)
 
         #self.total_sp_amp = 0.35 #Old value so that we don't see higher harmonics when driving with 2 tones
         self.total_sp_amp = 1 # Gets more optical power
 
-        self.dds_delay = 200*ms#0#1*ms #Enough time to let the DDS update, program long pulseshapes, and hopefully prevent crashes
-
-        self.pulse_shape_duration = 2*us
 
     @kernel
     def set_to_profile(self,channel,profile,delay=True):
@@ -129,58 +131,55 @@ class RamanInterface:
         """Rounds to nearest LSB freq of the DDS, i.e. the actual frequency produced by the DDS. """
         return int(round(freq/self.lsb))*self.lsb
 
-    def set_profile(self, channel, frequency, profile=1, amplitude=1, phase=0):
+    def set_profile(self, channel, frequency, profile=1, amplitude=1, phase=0, add_qubit_freq=True, on_clock=False):
         """Set profile"""
         if channel == 'rPara':
-            self.rPara.set(frequency,profile=profile, amplitude=amplitude, phase=phase)
-            #self.rPara.dds.set_sensible_pulse_shape(self.pulse_shape_duration)
+            self.rPara.set(frequency,profile=profile, amplitude=amplitude, phase=phase, add_qubit_freq=add_qubit_freq, on_clock=on_clock)
             self.rPara.identity()
 
         elif channel == 'rH2':
             self.rH2.set(frequency,profile=profile, amplitude=amplitude, phase=phase)
+            self.rH2.identity()
 
+        elif channel == 'rV':
+            self.rV.set(frequency,profile=profile, amplitude=amplitude, phase=phase)
+            self.rV.identity()
 
 
     def make_safe(self):
-        """Prevents second channel connected to SP AOM outputting RF, which may cause total RF power to exceed the AOM's damage threshold"""
+        """Prevents second channel connected to bichromatic AOM outputting RF, which may cause total RF power to exceed the AOM's damage threshold"""
         for i in range(8):
-            self.rH2.set2(0, profile = i, amplitude = 0)
-        self.rH2.set(0, profile = 1, amplitude = 0)
+            self.rPara.set2(0, profile = i, amplitude = 0)
+        self.rPara.set(0, profile = 1, amplitude = 0)
 
     @kernel
-    def set_sensible_pulse_shape(self):
-        self.rPara.dds.set_sensible_pulse_shape(self.pulse_shape_duration) #takes about 200ms
+    def set_sensible_pulse_shape(self,pulse_shape_duration=2*us):
+        self.rH2.dds.set_sensible_pulse_shape(pulse_shape_duration) #takes about 200ms
+        self.rH2.identity()
 
     @kernel
     def pulse_shape_on(self):
-        self.rPara.dds.pulse_enable(1)
+        self.rH2.dds.pulse_enable(1)
 
     @kernel
     def pulse_shape_off(self):
-        self.rPara.dds.pulse_enable(0)
+        self.rH2.dds.pulse_enable(0)
 
-    # def set_bichromat(self,sideband_freq, phase = 0, centre_freq=0, RSB_amp = None, BSB_amp = None):
-    #     """Sets up the dds channels to output a symmetric bicromatic tone on the SP AOM, with the DP AOM at the centre freq. This sets the DP profile 0 to "centre_freq" and "phase", and the SP first channel, profile 0 to it's centre """
+    def set_bichromat(self,sideband_freq, phase = 0, rPara_profile=1, rParaB_profile=0, RSB_amp = None, BSB_amp = None):
+        """Sets up the dds channels to output a symmetric bi-chromatic tone on the rPara AOM"""
 
-    #     imbalance_param = 1.04
+        imbalance_param = 1.00
 
-    #     RSB_amp_default = (imbalance_param/np.sqrt(2))*self.total_sp_amp
-    #     BSB_amp_default = np.sqrt(1 - (imbalance_param**2)/2)*self.total_sp_amp
+        RSB_amp_default = (imbalance_param/np.sqrt(2))*self.total_sp_amp
+        BSB_amp_default = np.sqrt(1 - (imbalance_param**2)/2)*self.total_sp_amp
 
-    #     RSB_amp = RSB_amp_default if RSB_amp is None else RSB_amp
-    #     BSB_amp = BSB_amp_default if BSB_amp is None else BSB_amp
+        RSB_amp = RSB_amp_default if RSB_amp is None else RSB_amp
+        BSB_amp = BSB_amp_default if BSB_amp is None else BSB_amp
 
-    #     assert(np.sqrt(RSB_amp**2 + BSB_amp**2) <= 1.0)
+        assert(np.sqrt(RSB_amp**2 + BSB_amp**2) <= 1.0)
 
-    #     self.dp.set(centre_freq,phase=phase) # set DP AOM at centre of bichromatic field on profile 0
+        self.rPara.set ( sideband_freq,profile=rPara_profile,  amplitude = BSB_amp, phase=phase, add_qubit_freq=False) #BSB
+        self.rPara.set2(-sideband_freq,profile=rParaB_profile, amplitude = RSB_amp, phase=phase, add_qubit_freq=False) #RSB
 
-    #     rounded_sideband_freq = self._lsb_round(sideband_freq) # To ensure RSB and BSB are not rounded differently
-
-    #     assert(self._lsb_round(self.sp.centre) == (self._lsb_round(self.sp.centre+rounded_sideband_freq) + self._lsb_round(self.sp.centre-rounded_sideband_freq))/2 )
-
-    #     self.sp.set(rounded_sideband_freq,profile=1, amplitude = BSB_amp) #BSB
-    #     self.sp.set2(-rounded_sideband_freq,profile=0, amplitude = RSB_amp) #RSB
-    #     self.dp.dds.set_sensible_pulse_shape(self.pulse_shape_duration)
-
-        # time.sleep(self.dds_delay)
+        self.rPara.identity() # check if finished
 
