b04f7b5fcee221816f2bb8d6723be5c61b502039
Blade master <ion@allxdaq54.nat.physics.ox.ac.uk>
2020-08-11 17:32:47 +0100

    raman interface: attempt to add rHSr

--- artiqDrivers/devices/coherentDds/raman_interface.py
+++ artiqDrivers/devices/coherentDds/raman_interface.py
@@ -13,7 +13,7 @@ class AOM:
         order: sum of the orders of each pass (i.e. double pass both in -1 order is -2)
         ttl: ttl device switching RF to AOM (or at least one of the channels)
         dds: first channel connected to AOM
-        
+
         no longer used: dds2: second channel connected to AOM (if any)
         this caused problems with default NoneType vs DdsChannel type on the kernel
 
@@ -51,14 +51,17 @@ class RamanAOM(AOM):
         self.ms_diff = 3.2e9 # master-slave Raman laser frequency difference
         self.rH_freq = -109e6 # frequency of Rh, is -1st order, =self.rV_freq+hfq.df_trans(mF4=4,mF3=3)-self.ms_diff
         self.rH2_freq = 217.309632e6 # frequency of Rh2, is +1st order, =self.rV_freq+hfq.df_trans(mF4=0,mF3=1)-self.ms_diff
+        self.rHSr_freq = -192.0597339091928e6 # sr qubit freq = sr88.s_1_2_splitting(b_field=146.0942e-4) = 409428288.9091928, rHSr_freq = rV_freq - sr qubit freq # TODO: might need to add 6 kHz here like for sr_rf? # in set_dds, freq_dds = freq/order, so needs to be -ve here
 
         super().__init__(name,freq_range,order,dds)
 
-    def calculate_dds_frequency(self,frequency,add_qubit_freq=True,on_clock=False):
+    def calculate_dds_frequency(self,frequency,add_qubit_freq=True,on_clock=False,on_sr=False):
         if (self.name == 'rPara') or (self.name == 'rParaB'):
             if add_qubit_freq:
                 if on_clock:
                     freqDDS = self.ms_diff+self.rH2_freq-frequency
+                elif on_sr:
+                    freqDDS = self.rV_freq-self.rHSr_freq+frequency
                 else:
                     freqDDS = self.ms_diff+self.rH_freq-frequency
             else: # this is used for gates, where the laser detuning is only the motional mode frequency + delta_g
@@ -68,14 +71,23 @@ class RamanAOM(AOM):
             freqDDS = self.rH2_freq
         elif self.name == 'rV':
             freqDDS = self.rV_freq
+        elif self.name == 'rHSr':
+            if on_sr:
+                if add_qubit_freq:
+                    freqDDS = self.rV_freq - frequency # -1 order so subtract freq
+                else:
+                    freqDDS = self.rHSr_freq - frequency # -1 order so subtract freq
+            else:
+                freqDDS =
+            print("rHSr DDS freq = ",freqDDS)
         else:
             raise ValueError("{} not a valid DDS channel name".format(self.name))
         return freqDDS
 
 
-    def set(self, frequency, profile=0, amplitude=1, phase=0, add_qubit_freq=True, on_clock=False):
+    def set(self, frequency, profile=0, amplitude=1, phase=0, add_qubit_freq=True, on_clock=False, on_sr=False):
         #print('configure profile', self.name, "profile", profile, "phase", phase)
-        freqDDS = self.calculate_dds_frequency(frequency,add_qubit_freq=add_qubit_freq,on_clock=on_clock)
+        freqDDS = self.calculate_dds_frequency(frequency,add_qubit_freq=add_qubit_freq,on_clock=on_clock,on_sr=on_sr)
         super().set(frequency=freqDDS, profile=profile, amplitude=amplitude, phase=phase)
 
     def _direct_set(self, frequency, profile=0, amplitude=1, phase=0):
@@ -105,7 +117,8 @@ class RamanInterface:
         self.rPara = RamanAOM("rPara",[200e6, 250e6],+1,dds._ch1)
         self.rParaB = RamanAOM("rParaB",[200e6, 250e6],+1,dds._ch4)
         self.rH2 = RamanAOM("rH2",[175e6, 225e6],+1,dds._ch3) # single pass +1 order
-        self.rV = RamanAOM("rV",[175e6, 225e6],+1,dds._ch2)
+        #self.rV = RamanAOM("rV",[175e6, 225e6],+1,dds._ch2)
+        self.rHSr = RamanAOM("rHSr", [175e6, 225e6],-1,dds._ch2) # repurposed the rV 200 MHz aom in -1st order
 
         #self.total_sp_amp = 0.35 #Old value so that we don't see higher harmonics when driving with 2 tones
         self.total_sp_amp = 1 # Gets more optical power
@@ -120,6 +133,8 @@ class RamanInterface:
             self.rParaB.dds.use_profile(profile,delay = delay)
         elif channel == 'rH2':
             self.rH2.dds.use_profile(profile,delay = delay)
+        elif channel == "rHSr":
+            self.rHSr.dds.use_profile(profile,delay = delay)
         else:
             raise ValueError("set_to_profile() not implemented for channel")
 
@@ -129,25 +144,27 @@ class RamanInterface:
         #TODO check, which channels/ simultaneously? we need to switch channels
         self.rPara.dds.reset_phase()
         self.rH2.dds.reset_phase()
-        self.rV.dds.reset_phase()
+        #self.rV.dds.reset_phase()
+        self.rHSr.dds.reset_phase()
         self.rParaB.dds.reset_phase()
 
     def serial_reset_phase(self):
         self.rPara.dds.serial_reset_phase()
         self.rH2.dds.serial_reset_phase()
-        self.rV.dds.serial_reset_phase()
+        #self.rV.dds.serial_reset_phase()
+        self.rHSr.dds.serial_reset_phase()
         self.rParaB.dds.serial_reset_phase()
 
     def _lsb_round(self,freq):
         """Rounds to nearest LSB freq of the DDS, i.e. the actual frequency produced by the DDS. """
         return int(round(freq/self.lsb))*self.lsb
 
-    def set_profile(self, channel, frequency, profile=1, amplitude=1, phase=0, 
-                    add_qubit_freq=True, on_clock=False):
+    def set_profile(self, channel, frequency, profile=1, amplitude=1, phase=0,
+                    add_qubit_freq=True, on_clock=False, on_sr=False):
         """Set profile"""
         if channel == 'rPara':
-            self.rPara.set(self._lsb_round(frequency),profile=profile, amplitude=amplitude, phase=phase, 
-                            add_qubit_freq=add_qubit_freq, on_clock=on_clock)
+            self.rPara.set(self._lsb_round(frequency),profile=profile, amplitude=amplitude, phase=phase,
+                            add_qubit_freq=add_qubit_freq, on_clock=on_clock, on_sr=on_sr)
             self.rPara.identity()
 
         elif channel == 'rH2':
@@ -155,11 +172,16 @@ class RamanInterface:
             self.rH2.identity()
 
         elif channel == 'rV':
-            self.rV.set(frequency,profile=profile, amplitude=amplitude, phase=phase)
-            self.rV.identity()
+            raise ValueError("DDS channel rV repurposed as rHSr")
+            #self.rV.set(frequency,profile=profile, amplitude=amplitude, phase=phase)
+            #self.rV.identity()
+
+        elif channel == 'rHSr':
+            self.rHSr.set(self._lsb_round(frequency),profile=profile, amplitude=amplitude, phase=phase)
+            self.rHSr.identity()
 
         elif channel == 'rParaB':
-            self.rParaB.set(self._lsb_round(frequency),profile=profile, amplitude=amplitude, phase=phase, 
+            self.rParaB.set(self._lsb_round(frequency),profile=profile, amplitude=amplitude, phase=phase,
                             add_qubit_freq=add_qubit_freq, on_clock=on_clock)
             self.rParaB.identity()
 
@@ -176,9 +198,13 @@ class RamanInterface:
             self.rH2._direct_set(frequency,profile=profile)
             self.rH2.identity()
         if laser == 'rV':
-            self.rV._direct_set(frequency,profile=profile)
-            self.rV.identity()
-        if (laser != 'rPara') and (laser != 'rH2') and (laser != 'rV'):
+            raise ValueError("DDS channel rV repurposed as rHSr")
+            #self.rV._direct_set(frequency,profile=profile)
+            #self.rV.identity()
+        if laser == 'rHSr':
+            self.rHSr._direct_set(frequency,profile=profile)
+            self.rHSr.identity()
+        if (laser != 'rPara') and (laser != 'rH2') and (laser != 'rV') and (laser != 'rHSr'):
             raise ValueError("Unknown laser '{}'".format(laser))
 
     def make_safe(self):
@@ -213,11 +239,18 @@ class RamanInterface:
 
         imbalance_param = 0.8
 
+        #print("set bichromat, sideband freq = ",sideband_freq)
+
         RSB_amp_default = (imbalance_param/np.sqrt(2))*self.total_sp_amp
         BSB_amp_default = np.sqrt(1 - (imbalance_param**2)/2)*self.total_sp_amp
 
         RSB_amp = RSB_amp_default if RSB_amp is None else RSB_amp
         BSB_amp = BSB_amp_default if BSB_amp is None else BSB_amp
+
+        #print("RSB amp: ", RSB_amp)
+        #print("BSB_amp: ", BSB_amp)
+
+        print("raman set bichromat, sideband freq +/-",sideband_freq)
 
         rounded_sideband_freq = self._lsb_round(sideband_freq) # To ensure RSB and BSB are not rounded differently
 
